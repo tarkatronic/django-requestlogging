0dbbc657b3a034967bd3677e77d89ee90382c36b
Joey Wilhelm <jwilhelm@godaddy.com>
2016-10-13 17:28:18 -0700

    Update the setup to for the new home

--- setup.py
+++ setup.py
@@ -1,23 +1,33 @@
 #!/usr/bin/env python
-from setuptools import setup
+from setuptools import find_packages, setup
 
 setup(
-    name='django-requestlogging',
-    version='1.0',
+    name='django-requestlogging-redux',
+    version='1.2.0',
     description='Adds information about requests to logging records.',
     long_description=open('README.rst', 'r').read(),
     author='TrustCentric',
     author_email='admin@trustcentric.com',
-    url='http://bitbucket.org/trustcentric/django-requestlogging/',
+    maintainer='Joey Wilhelm',
+    maintainer_email='tarkatronic@gmail.com',
+    url='https://github.com/tarkatronic/django-requestlogging',
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Environment :: Web Environment',
         'Framework :: Django',
+        'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
     ],
-    packages=['django_requestlogging'],
-    zip_safe=True,
-    install_requires=['Django>=1.3']
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=['Django>=1.8'],
+    tests_require=['tox']
 )
